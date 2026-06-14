4f730d13d78493b8f5ed38f1240c3e75de2eb55b
Lucas Gelenski <gelenskilucas@gmail.com>
2026-06-14 17:04:11 -0300

    Adicionada interface gráfica para gerenciamento de serviços.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,10 @@ module org.barbertrack.barbertrack_poo {
     requires javafx.fxml;
 
     exports org.barbertrack.barbertrack_poo.app;
+    exports org.barbertrack.barbertrack_poo.views;
+    exports org.barbertrack.barbertrack_poo.model;
+    exports org.barbertrack.barbertrack_poo.repository;
 
     opens org.barbertrack.barbertrack_poo.app to javafx.fxml;
+    opens org.barbertrack.barbertrack_poo.views to javafx.fxml;
 }
--- src/main/java/org/barbertrack/barbertrack_poo/app/Main.java
+++ src/main/java/org/barbertrack/barbertrack_poo/app/Main.java
@@ -1,15 +1,29 @@
 package org.barbertrack.barbertrack_poo.app;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import org.barbertrack.barbertrack_poo.views.ServicoView;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
         stage.setTitle("BarberTrack");
-        stage.show();
+
+        Button btnServicos = new Button("Gerenciar Serviços");
+        btnServicos.setOnAction(e -> {
+            new ServicoView().start(new Stage());
+        });
+
+        VBox root = new VBox(10, btnServicos);
+        root.setPadding(new Insets(12));
+
+        stage.setScene(new Scene(root, 400, 300));
+        stage.show(); // só aqui
     }
 
     public static void main(String[] args) {
